c4730ce286af671c7c54d47237b8a565bbb827c7
puni9869 <punitinani1@hotmail.com>
2026-01-25 09:08:55 +0530

    fix: reducing the build size

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,23 @@ export default defineConfig({
   base: '/blank-editor/',
   build: {
     outDir: 'docs',
+    esbuild: {
+      drop: ['console', 'debugger'],
+    },
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+    terserOptions: {
+      compress: {
+        dead_code: true,
+        drop_console: true, // Example: Remove console.log statements
+        arrows: true,
+      },
+      mangle: true, // Example: Mangle variable names
+    },
+    minify: 'terser', // 'esbuild' (default), 'terser', or false to disable
+    cssMinify: 'lightningcss', // 'esbuild' (default) or 'lightningcss'
   },
 });
