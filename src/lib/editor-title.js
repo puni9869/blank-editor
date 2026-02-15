c79ec8631a5fc90aa858f8c9939d48880a6fa4d9
puni9869 <punitinani1@hotmail.com>
2026-02-15 12:44:09 +0530

    fix: adding clearing content with title as well

--- src/lib/editor-title.js
+++ src/lib/editor-title.js
@@ -1,12 +1,18 @@
-import { EDITOR_TITLE_KEY } from '../config/config.js';
+import {EDITOR_TITLE_KEY} from '../config/config.js';
 
 export function saveTitle(editor) {
-  if (!editor) {
-    return;
-  }
+	if (!editor) {
+		return;
+	}
 
-  const title = localStorage.getItem(EDITOR_TITLE_KEY);
+	const title = localStorage.getItem(EDITOR_TITLE_KEY);
 
-  const titleEl = document.getElementById('title');
-  titleEl.value = title || editor.getText()?.trim()?.slice(0, 15);
+	const titleEl = document.getElementById('title');
+	titleEl.value = title || editor.getText()?.trim()?.slice(0, 15);
+}
+
+export function clearTitleData() {
+	localStorage.removeItem(EDITOR_TITLE_KEY);
+	document.getElementById('title').value = "";
+	console.log("Clieked");
 }
